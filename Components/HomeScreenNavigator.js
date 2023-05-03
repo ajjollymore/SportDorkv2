92afe6fdd1901f99c7b906b7e128031fa43107e4
Ajollymore <ajjollymore@gmail.com>
2023-05-03 12:42:23 -0300

    majority minority added to navigator

--- Components/HomeScreenNavigator.js
+++ Components/HomeScreenNavigator.js
@@ -4,18 +4,21 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './HomeScreen'
 import MavGameCards from '../Screens/MavGameCards'
-
+import BetScreen from '../Screens/PostSwipeBets'
+import MajorityMinority from '../Screens/MajorityMinority'
 const HomeScreenNavigator = () => {
     const Stack = createStackNavigator()
   return (
     <Stack.Navigator
     screenOptions={{
-        headerShown: false
+        headerShown: false,
+        cardStyle: {backgroundColor: 'white'}
     }
     }
     >
         <Stack.Screen name = "HomeStack" component={HomeScreen} />
         <Stack.Screen name = "MavGameCards" component={MavGameCards}/>
+        <Stack.Screen name = "MajorityMinority" component={MajorityMinority}/>
     </Stack.Navigator>
   )
 }
